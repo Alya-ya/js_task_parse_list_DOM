ccae8c4b5233bee3de964c109bab6fdc5aec8a2c
Alya <alinataranik03@gmail.com>
2025-07-31 21:43:32 +0300

    add task solution 2 31.07.25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,13 +24,15 @@ function getEmployees(items) {
   for (const item of items) {
     const employeeName = item.dataset.name;
     const position = item.dataset.position;
-    const salar = Number(item.dataset.salary.replace('$', '').replace(',', ''));
+    const salary = Number(
+      item.dataset.salary.replace('$', '').replace(',', ''),
+    );
     const age = item.dataset.age;
 
     result.push({
       employeeName,
       position,
-      salar,
+      salary,
       age,
     });
   }
